c4d5d38319c2f3e8616314bcfbfafbabe4b23893
Lesthy <103683378+Lesthy@users.noreply.github.com>
2022-06-30 20:31:06 +0300

    Added functionality for reading patient info from file

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        PatientMenu menu = new PatientMenu();
+
+        menu.skel();
+
+    }
+}
---
+++ src/PatientList.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PatientList extends javax.swing.JFrame {
+
+//    public Component pList() {
+//
+//        DefaultListModel<String> pacienti = new DefaultListModel<>();
+//        pacienti.addElement("John White");
+//        pacienti.addElement("Arthur Snow");
+//        pacienti.addElement("Logan Paix");
+//        pacienti.addElement("Rick Johnson");
+//        pacienti.addElement("Jack Silver");
+//
+//        JList<String> plist = new JList<>(pacienti);
+//        plist.setPreferredSize(new Dimension(250, 100));
+//
+//        return null;
+//    }
+
+}
