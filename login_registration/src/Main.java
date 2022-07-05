4bd63543c9cdc016f90280c67283c4b374113245
rozolins9 <r.ozolins9@gmail.com>
2022-07-05 15:18:16 +0300

    pievienots login

--- login_registration/src/Main.java
+++ login_registration/src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        Login login = new Login(null);
         Registration reg = new Registration(null);
         User user = reg.user;
         if (user != null) {
